66c29ca9900a4d49c674569d4369104efb202cbf
LUKAS U IDIOT <lukas.neumanntuf@gmail.com>
2022-05-22 14:59:47 +0200

    initial commit

---
+++ ui/qt_core.py
@@ -0,0 +1,5 @@
+from PySide6.QtWidgets import *
+from PySide6.QtUiTools import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6 import QtCore
---
+++ ui/window.py
@@ -0,0 +1,22 @@
+import sys
+from qt_core import *
+from qt_material import apply_stylesheet
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        loader = QUiLoader()
+        self.ui = loader.load("ui_files/main.ui")
+
+        self.setCentralWidget(self.ui)
+    
+def run_ui():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    apply_stylesheet(app, "dark_teal.xml")
+
+    window.show()
+    app.exec()
